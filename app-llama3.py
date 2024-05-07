b1996166df19c9899d62fe5f081cc673da5dec65
Gleisson de Assis <gleisson.assis@gmail.com>
2024-05-07 19:58:56 -0300

    initial copy

---
+++ app-llama3.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+from ctransformers import AutoModelForCausalLM
+
+llm = AutoModelForCausalLM.from_pretrained(
+    "TheBloke/Llama-2-7b-Chat-GGUF", model_file="llama-2-7b-chat.Q5_K_M.gguf"
+)
+
+# Dicionário para armazenar o histórico de conversas, limitado a 20 respostas por thread_id
+conversation_history = {}
+
+def get_prompt(system: str, instruction: str, thread_id: str) -> str:
+    history = conversation_history.get(thread_id, [])
+    history_text = " ".join(history)
+    prompt = f"<s>[INST] <<SYS>>\n{system}\n<</SYS>>\n\n"
+    if history:
+        prompt += f"This is the conversation history: {history_text}. Now answer the question: "
+    prompt += f"{instruction}\n\n### Response:\n [/INST]"
+    print(f"Prompt created: {prompt}")
+    return prompt
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bem-vindo à API de demonstração!"
+
+@app.route('/deebff5f-4fbf-4f06-bcb6-f54a2771c96f/chat', methods=['POST'])
+def process_json():
+    data = request.get_json()  # Recebe o JSON do corpo da requisição
+    
+    # Extrai dados do payload JSON
+    system = data.get('system', '')
+    instruction = data.get('instruction', '')
+    thread_id = data.get('thread_id', '')  # Parâmetro thread_id obrigatório
+
+    if not thread_id:
+        return jsonify({"error": "thread_id is required"}), 400
+
+    # Gera o prompt e obtém a resposta do modelo
+    prompt = get_prompt(system, instruction, thread_id)
+    response = llm(prompt)
+
+    # Atualiza o histórico da conversa para este thread_id
+    if thread_id in conversation_history:
+        if len(conversation_history[thread_id]) >= 20:
+            conversation_history[thread_id].pop(0)  # Remove a resposta mais antiga se o limite for alcançado
+        conversation_history[thread_id].append(response)
+    else:
+        conversation_history[thread_id] = [response]
+
+    # Retorna a resposta junto com o thread_id
+    return jsonify({"response": response, "thread_id": thread_id})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
